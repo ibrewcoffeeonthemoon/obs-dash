19b9de94e9f16113610130e4e42a3eb180d2c045
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-22 23:31:06 +0800

    even bigger timer

--- components/App/(tabs)/recording/RecordingTime.tsx
+++ components/App/(tabs)/recording/RecordingTime.tsx
@@ -5,18 +5,18 @@ export const RecordingTime = () => {
   return (
     <ThemedView
       style={{
-        paddingHorizontal: 20,
+        paddingHorizontal: 10,
         paddingVertical: 50,
       }}
     >
       <ThemedText
         style={{
-          fontSize: 120,
-          lineHeight: 120,
+          fontSize: 200,
+          lineHeight: 200,
           textAlign: "center",
         }}
       >
-        00:10:25
+        10:25
       </ThemedText>
       ;
     </ThemedView>
