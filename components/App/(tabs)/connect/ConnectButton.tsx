c54be94078c438ddd2305bd91253f16422bf6885
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-22 22:53:55 +0800

    single button connect/disconnect

--- components/App/(tabs)/connect/ConnectButton.tsx
+++ components/App/(tabs)/connect/ConnectButton.tsx
@@ -12,6 +12,8 @@ export const ConnectButton = () => {
   const ipAddress = useStore((s) => s.state.ipAddress);
   const port = useStore((s) => s.state.port);
   const password = useStore((s) => s.state.password);
+  const isConnected = useStore((s) => s.stash.isConnected);
+  const setIsConnected = useStore((s) => s.action.setIsConnected);
   const appendLog = useStore((s) => s.action.appendLog);
 
   const connectOBS = async () => {
@@ -19,31 +21,36 @@ export const ConnectButton = () => {
       appendLog("Connecting...");
       await obs.connect(`ws://${ipAddress}:${port}`, password);
       appendLog("Connected to OBS");
+      setIsConnected(true);
     } catch (error) {
       appendLog("Failed to connect to OBS");
+      setIsConnected(false);
     }
   };
   const disconnectOBS = async () => {
     appendLog("Disconnecting...");
     await obs.disconnect();
     appendLog("Disconnected to OBS");
+    setIsConnected(false);
   };
 
   return (
     <ThemedView style={styles.stepContainer}>
-      <TouchableOpacity
-        style={{ ...styles.button, backgroundColor: "green" }}
-        onPress={connectOBS}
-      >
-        <ThemedText style={styles.buttonText}>Connect OBS</ThemedText>
-      </TouchableOpacity>
-
-      <TouchableOpacity
-        style={{ ...styles.button, backgroundColor: "red" }}
-        onPress={disconnectOBS}
-      >
-        <ThemedText style={styles.buttonText}>Disconnect OBS</ThemedText>
-      </TouchableOpacity>
+      {!isConnected ? (
+        <TouchableOpacity
+          style={{ ...styles.button, backgroundColor: "green" }}
+          onPress={connectOBS}
+        >
+          <ThemedText style={styles.buttonText}>Connect</ThemedText>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          style={{ ...styles.button, backgroundColor: "red" }}
+          onPress={disconnectOBS}
+        >
+          <ThemedText style={styles.buttonText}>Disconnect</ThemedText>
+        </TouchableOpacity>
+      )}
     </ThemedView>
   );
 };
