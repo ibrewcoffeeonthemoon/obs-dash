60fcb59262faebdcd361035c2e137687e8778e86
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-22 22:26:54 +0800

    cleanup

--- components/App/(tabs)/connect/index.tsx
+++ components/App/(tabs)/connect/index.tsx
@@ -1,13 +1,6 @@
-import { ScrollView, TouchableOpacity } from "react-native";
-import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
-import { useEffect, useRef } from "react";
-import { OBSWebSocket } from "obs-websocket-js";
-import { ThemedTextInput } from "@/components/themed-textinput";
-import { styles } from "./styles";
 import { Banner } from "./Banner";
 import { Title } from "./Title";
-import { useStore } from "@/store/connect";
 import { IpAddressInput } from "./IpAddressInput";
 import { PortInput } from "./PortInput";
 import { PasswordInput } from "./PasswordInput";
@@ -15,22 +8,6 @@ import { ConnectButton } from "./ConnectButton";
 import { LogArea } from "./LogArea";
 
 export default function Connect() {
-  const ipAddress = useStore((s) => s.state.ipAddress);
-  const setIpAddress = useStore((s) => s.action.setIpAddress);
-
-  const port = useStore((s) => s.state.port);
-  const setPort = useStore((s) => s.action.setPort);
-
-  const password = useStore((s) => s.state.password);
-  const setPassword = useStore((s) => s.action.setPassword);
-
-  const showPassword = useStore((s) => s.stash.showPassword);
-  const setShowPassword = useStore((s) => s.action.setShowPassword);
-
-  const log = useStore((s) => s.stash.log);
-  const appendLog = useStore((s) => s.action.appendLog);
-  const clearLog = useStore((s) => s.action.clearLog);
-
   return (
     <>
       <Banner />
