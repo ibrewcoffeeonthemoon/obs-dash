fe3308cda1870c57b560e3b0c9602019567cc081
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-22 23:04:04 +0800

    factor out styles

--- components/App/(tabs)/recording/index.tsx
+++ components/App/(tabs)/recording/index.tsx
@@ -1,22 +1,9 @@
-import { StyleSheet } from "react-native";
 import ParallaxScrollView from "@/components/parallax-scroll-view";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { IconSymbol } from "@/components/ui/icon-symbol";
 import { Fonts } from "@/constants/theme";
-
-const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
-  },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 8,
-  },
-});
+import { styles } from "./styles";
 
 export default function Recording() {
   return (
