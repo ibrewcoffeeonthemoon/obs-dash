763535186c56680fd1dc99a42288fcb2380f38d0
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-22 23:08:45 +0800

    simplify continer, factor out Banner

---
+++ components/App/(tabs)/recording/Banner.tsx
@@ -0,0 +1,16 @@
+import { ThemedView } from "@/components/themed-view";
+import { styles } from "./styles";
+import { IconSymbol } from "@/components/ui/icon-symbol";
+
+export const Banner = () => {
+  return (
+    <ThemedView style={{ ...styles.titleContainer, height: 150 }}>
+      <IconSymbol
+        size={310}
+        color="#808080"
+        name="chevron.left.forwardslash.chevron.right"
+        style={styles.headerImage}
+      />
+    </ThemedView>
+  );
+};
--- components/App/(tabs)/recording/index.tsx
+++ components/App/(tabs)/recording/index.tsx
@@ -4,33 +4,33 @@ import { ThemedView } from "@/components/themed-view";
 import { IconSymbol } from "@/components/ui/icon-symbol";
 import { Fonts } from "@/constants/theme";
 import { styles } from "./styles";
+import { Banner } from "./Banner";
 
 export default function Recording() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{
-            fontFamily: Fonts.rounded,
-          }}
-        >
-          Recording Control
-        </ThemedText>
+    <>
+      <Banner />
+      <ThemedView
+        style={{
+          flex: 1,
+          padding: 32,
+          gap: 16,
+        }}
+      >
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText
+            type="title"
+            style={{
+              fontFamily: Fonts.rounded,
+            }}
+          >
+            Recording Control
+          </ThemedText>
+        </ThemedView>
+        <ThemedText>Quick Toggle to start/pause/stop OBS recording</ThemedText>
+        <ThemedText type="subtitle">Show recording time</ThemedText>
+        <ThemedText type="subtitle">Start/Pause/Stop button</ThemedText>
       </ThemedView>
-      <ThemedText>Quick Toggle to start/pause/stop OBS recording</ThemedText>
-      <ThemedText type="subtitle">Show recording time</ThemedText>
-      <ThemedText type="subtitle">Start/Pause/Stop button</ThemedText>
-    </ParallaxScrollView>
+    </>
   );
 }
