95113c00173f116f6896f05db298d1edf1e658e4
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-23 12:22:35 +0800

    added isFocused check

--- components/App/(tabs)/recording/KeepAwakeButton.tsx
+++ components/App/(tabs)/recording/KeepAwakeButton.tsx
@@ -1,30 +1,26 @@
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { useStore } from "@/store/recording";
+import { useIsFocused } from "@react-navigation/native";
 import { activateKeepAwakeAsync, deactivateKeepAwake } from "expo-keep-awake";
 import { useEffect } from "react";
 import { Switch } from "react-native";
 
 export const KeepAwakeButton = () => {
+  const isFocused = useIsFocused();
   const isRecording = useStore((s) => s.stash.isRecording);
   const isWakeLockEnabled = useStore((s) => s.state.isWakeLockEnabled);
   const setIsWakeLockEnabled = useStore((s) => s.action.setIsWakeLockEnabled);
 
   useEffect(() => {
-    if (isRecording && isWakeLockEnabled) {
+    if (isFocused && isRecording && isWakeLockEnabled) {
       activateKeepAwakeAsync();
       console.log("activate");
     } else {
       deactivateKeepAwake();
       console.log("deactivate");
     }
-
-    // deactivate when component unmounted
-    return () => {
-      deactivateKeepAwake();
-      console.log("deactivate cleanup");
-    };
-  }, [isRecording, isWakeLockEnabled]);
+  }, [isFocused, isRecording, isWakeLockEnabled]);
 
   return (
     <ThemedView style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
