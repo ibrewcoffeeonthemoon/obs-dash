f49e0ba4e81a73de42582b79d5816295b3377bef
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-23 22:31:41 +0800

    ConnectButton when press get init and listen to RecordState change

--- components/App/(tabs)/connect/ConnectButton.tsx
+++ components/App/(tabs)/connect/ConnectButton.tsx
@@ -5,6 +5,7 @@ import { ThemedText } from "@/components/themed-text";
 import { useStore } from "@/store/connect";
 import { obs } from "@/lib/obs";
 import { OBSWebSocketError } from "obs-websocket-js";
+import { stores } from "@/store";
 
 export const ConnectButton = () => {
   const ipAddress = useStore((s) => s.state.ipAddress);
@@ -13,9 +14,13 @@ export const ConnectButton = () => {
   const isConnected = useStore((s) => s.stash.isConnected);
   const setIsConnected = useStore((s) => s.action.setIsConnected);
   const appendLog = useStore((s) => s.action.appendLog);
+  const setIsRecording = stores.recording.useStore(
+    (s) => s.action.setIsRecording,
+  );
 
   const connectOBS = async () => {
     try {
+      // connect
       const { obsWebSocketVersion, negotiatedRpcVersion } = await obs.connect(
         `ws://${ipAddress}:${port}`,
         password,
@@ -25,6 +30,13 @@ export const ConnectButton = () => {
         `Connected to server ${obsWebSocketVersion} (using RPC ${negotiatedRpcVersion})`,
       );
       setIsConnected(true);
+
+      // get and listen to recording state
+      const { outputActive } = await obs.call("GetRecordStatus");
+      setIsRecording(outputActive);
+      obs.on("RecordStateChanged", ({ outputActive }) => {
+        setIsRecording(outputActive);
+      });
     } catch (error) {
       if (error instanceof OBSWebSocketError) {
         appendLog(`Failed to connect, ${error.code}, ${error.message}`);
