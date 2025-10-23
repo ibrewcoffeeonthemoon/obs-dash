489e9fd38e0b13244629126824e6aa5020878e64
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-23 12:13:50 +0800

    check if is recording, and cleanup deactivate

--- components/App/(tabs)/recording/KeepAwakeButton.tsx
+++ components/App/(tabs)/recording/KeepAwakeButton.tsx
@@ -6,16 +6,25 @@ import { useEffect } from "react";
 import { Switch } from "react-native";
 
 export const KeepAwakeButton = () => {
+  const isRecording = useStore((s) => s.stash.isRecording);
   const isWakeLockEnabled = useStore((s) => s.state.isWakeLockEnabled);
   const setIsWakeLockEnabled = useStore((s) => s.action.setIsWakeLockEnabled);
 
   useEffect(() => {
-    if (isWakeLockEnabled) {
+    if (isRecording && isWakeLockEnabled) {
       activateKeepAwakeAsync();
+      console.log("activate");
     } else {
       deactivateKeepAwake();
+      console.log("deactivate");
     }
-  }, [isWakeLockEnabled]);
+
+    // deactivate when component unmounted
+    return () => {
+      deactivateKeepAwake();
+      console.log("deactivate cleanup");
+    };
+  }, [isRecording, isWakeLockEnabled]);
 
   return (
     <ThemedView style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
