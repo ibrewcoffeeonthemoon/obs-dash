deaf131dac22718018eaea662ef364a1c9c729a0
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-23 19:17:13 +0800

    fix banner height, no need to avoid keyboard if space is enough

--- components/App/(tabs)/connect/Banner.tsx
+++ components/App/(tabs)/connect/Banner.tsx
@@ -1,10 +1,15 @@
 import { Image } from "expo-image";
 import { ThemedView } from "@/components/themed-view";
 import { styles } from "./styles";
+import { stores } from "@/store";
 
 export const Banner = () => {
+  const isPhone = stores.app.useStore((s) => s.stash.isPhone);
+
   return (
-    <ThemedView style={{ ...styles.titleContainer, height: 125 }}>
+    <ThemedView
+      style={{ ...styles.titleContainer, height: isPhone ? 70 : 150 }}
+    >
       <Image
         source={require("@/assets/images/partial-react-logo.png")}
         style={styles.reactLogo}
