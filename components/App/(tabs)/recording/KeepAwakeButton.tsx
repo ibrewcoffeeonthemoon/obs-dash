073716945d564187f180c8aad3fcc1d19ec30538
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-23 11:38:59 +0800

    apply to the switch

--- components/App/(tabs)/recording/KeepAwakeButton.tsx
+++ components/App/(tabs)/recording/KeepAwakeButton.tsx
@@ -1,5 +1,6 @@
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
+import { useStore } from "@/store/recording";
 import {
   useKeepAwake,
   activateKeepAwake,
@@ -8,10 +9,12 @@ import {
 import { Switch } from "react-native";
 
 export const KeepAwakeButton = () => {
+  const isWakeLockEnabled = useStore((s) => s.state.isWakeLockEnabled);
+  const setIsWakeLockEnabled = useStore((s) => s.action.setIsWakeLockEnabled);
+
   return (
     <ThemedView style={{ flexDirection: "row", alignItems: "center", gap: 8 }}>
-      {/* <Switch value={isWakeLockEnabled} onValueChange={setIsWakeLockEnabled} /> */}
-      <Switch />
+      <Switch value={isWakeLockEnabled} onValueChange={setIsWakeLockEnabled} />
       <ThemedText>Keep screen awake during recording</ThemedText>
     </ThemedView>
   );
