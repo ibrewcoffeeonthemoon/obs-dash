7c5953480828cc032da7ffac7663e4243db85b85
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-24 23:55:17 +0800

    apply text: Connect To OBS Websocket

--- components/App/(tabs)/connect/Title.tsx
+++ components/App/(tabs)/connect/Title.tsx
@@ -1,16 +1,18 @@
 import { ThemedView } from "@/components/themed-view";
 import { styles } from "./styles";
-import { ThemedText } from "@/components/themed-text";
 import { stores } from "@/store";
+import { Text } from "react-native";
 
 export const Title = () => {
   const isPhone = stores.app.useStore((s) => s.stash.isPhone);
 
   return (
     <ThemedView style={styles.titleContainer}>
-      <ThemedText type={isPhone ? "subtitle" : "title"}>
+      <Text
+        className={`text-white ${isPhone ? "text-2xl" : "text-4xl"} font-bold`}
+      >
         Connect To OBS Websocket
-      </ThemedText>
+      </Text>
     </ThemedView>
   );
 };
