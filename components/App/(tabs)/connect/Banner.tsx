d1d8c87bf5629f2b8d9e3efb40aa1ddaaa0bacae
Ibrewcoffee Onthemoon <ibrewcoffeeonthemoon@gmail.com>
2025-10-25 00:09:25 +0800

    container

--- components/App/(tabs)/connect/Banner.tsx
+++ components/App/(tabs)/connect/Banner.tsx
@@ -1,19 +1,19 @@
 import { Image } from "expo-image";
-import { ThemedView } from "@/components/themed-view";
 import { styles } from "./styles";
 import { stores } from "@/store";
+import { View } from "react-native";
 
 export const Banner = () => {
   const isPhone = stores.app.useStore((s) => s.stash.isPhone);
 
   return (
-    <ThemedView
-      style={{ ...styles.titleContainer, height: isPhone ? 70 : 150 }}
+    <View
+      className={`flex flex-row items-center gap-2 ${isPhone ? "h-20" : "h-42"}`}
     >
       <Image
         source={require("@/assets/images/partial-react-logo.png")}
         style={styles.reactLogo}
       />
-    </ThemedView>
+    </View>
   );
 };
